86895f22eb5135e95f8328107832d41ec8fac3ff
Samuel Raphael <sammysgame.dev@gmail.com>
2021-09-18 20:05:37 +0100

    tk: initial push

---
+++ main.py
@@ -0,0 +1,24 @@
+# from data_structures.dynamic_array import DynamicArray
+#
+# list_ = DynamicArray()
+# list_.append(1)
+# list_.append(2)
+# list_.append(3)
+# list_.append(4)
+# list_.append(9)
+# print(f'length => {len(list_)}')
+# print()
+# for i in list_:
+#   print(f'element => {i}')
+#
+# print(list_[0])
+
+# from algorithms.binary_search import binary_search
+
+# list_ = [2, 4, 5, 7, 8, 9, 12, 14, 17, 19, 22, 25, 27, 28, 33, 37]
+# print(binary_search(
+#   list_,
+#   22,
+#   0,
+#   len(list_) - 1
+# ))
